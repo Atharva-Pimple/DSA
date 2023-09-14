6141e78faef88c2c3c43cd348a7bdcd1f6d8e1b9
Atharva Pimple <atharvapimple30@gmail.com>
2023-09-14 11:47:57 +0530

    Binary tree traversal

--- binaryTree/BinaryTreeA.java
+++ binaryTree/BinaryTreeA.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class BinaryTreeA{
     static class Node{
         int data;
@@ -25,12 +27,66 @@ public class BinaryTreeA{
 
             return newNode;
         }
+
+        public static void preOrder(Node root){
+            if(root==null){
+                return;
+            }
+            System.out.print(root.data+" ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+
+        public static void postOrder(Node root){
+            if(root==null){
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data+" ");
+        }
+
+        public static void levelOrder(Node root){
+            if(root==null){
+                return;
+            }
+
+            Queue<Node> q=new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()){
+                Node currNode=q.remove();
+                if(currNode==null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }else{
+                        q.add(null);
+                    }
+                }else{
+                    System.out.print(currNode.data+" ");
+
+                    if(currNode.left != null){
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right != null){
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
     }
 
     public static void main(String args[]){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree bt=new BinaryTree();
         Node root=bt.buildTree(nodes);
-        System.out.print(root.data);
+        // System.out.print(root.data);
+
+        // bt.preOrder(root);
+        // bt.postOrder(root);
+        bt.levelOrder(root);
+
     }
 }
