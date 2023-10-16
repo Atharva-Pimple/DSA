4b0638c05e69127f046e361e4711069059407f7c
Atharva Pimple <atharvapimple30@gmail.com>
2023-10-16 21:18:24 +0530

    Union and intersection using set

---
+++ hashing/hashSet/UnionAndIntersection.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class UnionAndIntersection{
+    public static void main(String args[]){
+        int arr1[]={7,3,9};
+        int arr2 []={6,3,9,2,9,4};
+
+        HashSet<Integer> set=new HashSet<>();
+        // Union
+        for(int i=0;i<arr1.length;i++){
+            set.add(arr1[i]);
+        }
+
+        for(int i=0;i<arr2.length;i++){
+            set.add(arr2[i]);
+        }
+
+        System.out.println("Union :"+set.size());
+
+        // Intersection
+        set=new HashSet<>();
+        for(int i=0;i<arr1.length;i++){
+            set.add(arr1[i]);
+        }
+
+        int count=0;
+        for(int i=0;i<arr2.length;i++){
+            if(set.contains(arr2[i])){
+                count++;
+                set.remove(arr2[i]);
+            }
+        }
+        
+        System.out.print("Intersection :"+count);
+    }
+}
