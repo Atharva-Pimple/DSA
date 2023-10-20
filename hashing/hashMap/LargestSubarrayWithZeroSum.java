a3d8b281e598fad49b1d3fd28f13b0acc81c6b18
Atharva Pimple <atharvapimple30@gmail.com>
2023-10-20 22:30:17 +0530

    Subarray problems using hashmap

---
+++ hashing/hashMap/LargestSubarrayWithZeroSum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class LargestSubarrayWithZeroSum{
+    public static void main(String args[]){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int arr[]={15,-2,2,-8,1,7,10,23};
+
+        int sum=0;
+        int len=0;
+        
+        for(int j=0;j<arr.length;j++){
+            sum += arr[j];
+            if(map.containsKey(sum)){
+                len=Math.max(len,j-map.get(sum));
+            }else{
+                map.put(sum,j);
+            }
+        } 
+
+        System.out.print("Largest subarray with sum 0: "+len);
+
+
+    }
+}
---
+++ hashing/hashMap/SubarraySumEqualK.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class SubarraySumEqualK{
+    public static void main(String args[]){
+        int arr[]={10,2,-2,-20,10};
+        int k=-10;
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+        // [sum,count]
+        map.put(0,1);
+
+        int sum=0;
+        int ans=0;
+
+        for(int i=0;i<arr.length;i++){
+            sum += arr[i];
+            if(map.containsKey(sum-k)){
+                ans += map.get(sum-k);
+            }
+
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+
+        System.out.print(ans);
+
+    }
+}
