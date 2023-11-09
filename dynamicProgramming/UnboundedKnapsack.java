f5927eaf2fac8b3fae68d4609aa87c60f5b8f0ad
Atharva Pimple <atharvapimple30@gmail.com>
2023-11-09 20:28:11 +0530

    Unbounded knapsack

--- dynamicProgramming/UnboundedKnapsack.java
+++ dynamicProgramming/UnboundedKnapsack.java
@@ -26,13 +26,13 @@ public class UnboundedKnapsack{
             }
         }
 
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                System.out.print(dp[i][j]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
+        // for(int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp[0].length;j++){
+        //         System.out.print(dp[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
 
         return dp[n][W];
 
