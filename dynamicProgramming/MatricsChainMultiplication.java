c691fb0ebc0c9040b8e424557b43d3f714490740
Atharva Pimple <atharvapimple30@gmail.com>
2023-11-22 17:29:37 +0530

    Matrics Chain Multiplication

--- dynamicProgramming/MatricsChainMultiplication.java
+++ dynamicProgramming/MatricsChainMultiplication.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class MatricsChainMultiplication{
     public static int mcm(int arr[],int i, int j){
         if(i==j){
@@ -37,19 +39,42 @@ public class MatricsChainMultiplication{
 
         return dp[i][j]=ans;
     }
+
+    public static int mcmTab(int arr[]){
+        int n=arr.length;
+        int dp[][]=new int[n][n];
+
+        for(int i=0;i<n;i++){
+            dp[i][i]=0;
+        }
+
+        for(int len=2;len<=n-1;len++){
+            for(int i=1;i<=n-len;i++){//row
+                int j=i+len-1;//col
+                dp[i][j]=Integer.MAX_VALUE;
+                for(int k=i;k<=j-1;k++){
+                    int cost1=dp[i][k];
+                    int cost2=dp[k+1][j];
+                    int cost3=arr[i-1]*arr[k]*arr[j];
+                    dp[i][j]=Math.min(dp[i][j],cost1+cost2+cost3);
+                }
+            }
+        }
+
+        return dp[1][n-1];
+    }
     public static void main(String args[]){
         int arr[]={1,2,3,4,3};
         int n=arr.length;
 
-        int dp[][]=new int [n+1][n+1];
+        int dp[][]=new int [n][n];
 
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j]=-1;
-            }
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
         }
 
         // System.out.print(mcm(arr,1,n-1));
-        System.out.print(mcmMemo(arr,1,n-1,dp));
+        // System.out.print(mcmMemo(arr,1,n-1,dp));
+        System.out.print(mcmTab(arr));
     }
 }
