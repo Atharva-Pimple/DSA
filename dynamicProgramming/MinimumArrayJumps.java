7b2beea2532aafdec40d067de99f9404100b624c
Atharva Pimple <atharvapimple30@gmail.com>
2023-11-27 15:40:11 +0530

    Minimum array jumps problem

---
+++ dynamicProgramming/MinimumArrayJumps.java
@@ -0,0 +1,26 @@
+public class MinimumArrayJumps{
+    public static int minjumps(int nums[]){
+        int n=nums.length;
+        int dp[]=new int[n];
+
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            int steps=nums[i];
+            int ans=Integer.MAX_VALUE;
+            for(int j=i+1;j<i+steps && j<n;j++){
+                if(dp[j]!=-1){
+                    ans=Math.min(ans,dp[j]+1);
+                }
+            }
+            if(ans != Integer.MAX_VALUE){
+                dp[i]=ans;
+            }
+        }
+
+        return dp[0];
+    }
+    public static void main(String args[]){
+        int arr[]={2,3,1,1,4};
+        System.out.print(minjumps(arr));
+    }
+}
