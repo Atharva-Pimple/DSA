7b1c53aa87a1b14ce7884c19c80084f8927df3fc
Atharva Pimple <atharvapimple30@gmail.com>
2023-11-30 14:54:01 +0530

    Segment tree methods

--- SegmentTrees/SegmengtTree.java
+++ SegmentTrees/SegmengtTree.java
@@ -17,6 +17,47 @@ public class SegmengtTree{
 
         tree[i]=tree[2*i+1]+tree[2*i+2];
     }
+
+    public static int getSumUtil(int i,int start,int end,int qi,int qj){
+        if(qj <= start || end <= qi){
+            return 0;
+        }else if(qi <= start && qj >=end){
+            return tree[i];
+        }else{
+            int mid=(start+end)/2;
+            int left=getSumUtil(2*i+1,start,mid,qi,qj);
+            int right=getSumUtil(2*i+2,mid+1,end,qi,qj);
+            return left+right;
+        }
+    }
+
+    public static int getSum(int arr[],int qi,int qj){
+        int n=arr.length;
+        return getSumUtil(0,0,n-1,qi,qj);
+    }
+
+    public static void updateUtil(int i,int start,int end, int idx, int diff){
+        if(start>idx || end<idx){
+            return;
+        }
+
+        tree[i] +=diff;
+        if(start != end){
+            int mid=(start+end)/2;
+            updateUtil(2*i+1,start,mid,idx,diff);
+            updateUtil(2*i+2,mid+1,end,idx,diff);
+        }
+    }
+
+    public static void update(int arr[],int idx,int val){
+        int n=arr.length;
+        int diff=val-arr[idx];
+        arr[idx]=val;
+
+        // Segment tree updation
+        updateUtil(0,0,n-1,idx,diff);
+
+    }
     public static void main(String args[]){
         int arr[]={1,2,3,4,5,6,7,8};
         int n=arr.length;
@@ -24,8 +65,15 @@ public class SegmengtTree{
 
         buildTree(arr,0,0,n-1);
 
-        for(int i=0;i<tree.length;i++){
-            System.out.print(tree[i]+" ");
-        }
+        // Print Tree
+
+        // for(int i=0;i<tree.length;i++){
+        //     System.out.print(tree[i]+" ");
+        // }
+
+        System.out.println(getSum(arr,2,5));
+        update(arr,2,2);
+        System.out.println(getSum(arr,2,5));
+        
     }
 }
